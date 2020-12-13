49927b3025d793e5990d3c145723b5d10b333269
Stefan <mail.ipv4v6+gh@gmail.com>
2020-12-13 21:54:05 +0100

    update download script to version 2020.12

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2020.11.3"
+VERSION="2020.12"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="b26663bf5933d38967f26c5b20770a3b0f1053b6dd503634cb30485fbbaac00d"
+SHA256="5acd1f77a4587de38dad8b64c746ddf606f97b0f0131f6b0c93a7ee2b531862f"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
