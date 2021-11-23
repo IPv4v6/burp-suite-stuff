2fec89d39a63eb7116c9354c1d778c1a7495abc0
Stefan <mail.ipv4v6+gh@gmail.com>
2021-11-23 23:08:25 +0100

    update download script to version 2021.10.2

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2021.10.1"
+VERSION="2021.10.2"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="e1d9ff3424d1cce65eb6eff396a705a3285de24a04a8db65ca9fe8ef7948ffa7"
+SHA256="02a0c87a0f13123d94f41a1e37e56774c868e1fdbeff22ee6bd2fbb667f51176"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
