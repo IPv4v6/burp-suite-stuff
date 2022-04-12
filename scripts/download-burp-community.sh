2f43b4a391ea4f774923a375108a8f11a35970ec
Stefan <mail.ipv4v6+gh@gmail.com>
2022-04-12 22:42:06 +0200

    update download script to version 2022.3.4

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2022.2.4"
+VERSION="2022.3.4"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="5def560d112803c4ad2dd4c33b1ff4b8fbc508da66bb9733a577c8377995938f"
+SHA256="31134ca2355b3777705db963fbdb121867e40e2bfcca3bc8a28553ae0355597d"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
