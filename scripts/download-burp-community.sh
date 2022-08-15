f3ea8e5a83611924160eb897a378bcbaabe5321b
Stefan <mail.ipv4v6+gh@gmail.com>
2022-08-15 22:01:00 +0200

    update download script to version 2022.8.1

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2022.7"
+VERSION="2022.8.1"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="a79856b146613253b7006b73245d896791729c4f61fcb772180fd5f55ee1d983"
+SHA256="590dd875554cb0472dac2ea2b429937b82c1dc1436595aee30ef87a9219ce4b7"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
