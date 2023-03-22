60e699a6fa7fffc91f1b4b4edae3c38bd0ff13a6
Stefan <mail.ipv4v6+gh@gmail.com>
2023-03-23 00:28:43 +0100

    update download script to version 2023.3

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2023.2.2"
+VERSION="2023.3"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="191d3e1ae0ba8d22a222ba9cb04c5400183e9d7cf3e189addf15558d50c766c0"
+SHA256="064c3e51e45fe3e8e9e7c1cc1af4417faac598bb31486f8eed65d1626b5867c2"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
