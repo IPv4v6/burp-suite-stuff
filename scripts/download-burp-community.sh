9ff8266d582046d3f3545f14121ddd3aefa6e0cd
Stefan <mail.ipv4v6+gh@gmail.com>
2024-02-01 00:44:02 +0100

    update download script to version 2024.1.1

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2023.12.1"
+VERSION="2024.1.1"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="2fb353d8c0743bbbd0064404a275e2ca51edeb628b11447fcd9faf47e960d973"
+SHA256="abb04139976b54b94d698129c3910230a94dd52c8f769cb72f002ee84bf6e532"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
