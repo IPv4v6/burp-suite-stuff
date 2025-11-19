5943cf774997d2c80dc9915d42959c3f37b0ee90
Stefan <mail.ipv4v6+gh@gmail.com>
2025-11-19 23:52:49 +0100

    update download script to version 2025.11.1

--- scripts/download-burp-community.sh
+++ scripts/download-burp-community.sh
@@ -3,9 +3,9 @@
 set -e
 set -u
 
-VERSION="2025.9.1"
+VERSION="2025.11.1"
 FILE="burpsuite_community_linux_v${VERSION}.sh"
-SHA256="71a607e467649e73bf6ae4296cd3b5a2010754f8b4ecba0ff1198c6cd565cf0a"
+SHA256="decaf0a86bacafdf47abfeef3cefb229976a96d0cc70394c7e87c1a5070ab90e"
 
 curl -R -o ${FILE} \
 "https://portswigger.net/burp/releases/download?product=community&version=${VERSION}&type=linux"
